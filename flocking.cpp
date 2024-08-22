8a0f924d4095abdb62beb07f9dbdb31aaa04b3af
ashom2 <alekid804@gmail.com>
2024-08-22 13:20:46 +1000

    Experimented with constructors.

--- flocking.cpp
+++ flocking.cpp
@@ -5,19 +5,33 @@ Vector2D class for storing two-dimensional spacial vectors.
 */
 class Vector2D { //TODO move to header
     public:
-    float x;
-    float y;
+        float x;
+        float y;
 
-    Vector2D(float x, float y) {
-        x = x;
-        y = y;
-    }
+        Vector2D() {
+            this->x = 0;
+            this->y = 0;
+        }
+
+        Vector2D(float x, float y) {
+            this->x = x;
+            this->y = y;
+        }
 };
 
 /*
 Main.
 */
 int main() {
-    std::cout << "Flocking/swarming\n";
+    //Vector2D arr[20];
+    Vector2D v1 = Vector2D(1.00, 2);
+
+    Vector2D arr[20];
+    for(int i=0; i<20; i++) {
+        arr[i] = Vector2D(-1, 5);
+    }
+
+    printf("%.2f", arr[0].x);
+    //std::cout << "Flocking/swarming\n" << roundf(v1.x * 100) / 100;
     return 0;
 }
