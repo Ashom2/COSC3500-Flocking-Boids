39327a9f8795210b9222af6c0559473ee5ad6797
ashom2 <alekid804@gmail.com>
2024-08-22 12:44:50 +1000

    Added Vector2D class.

--- flocking.cpp
+++ flocking.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+/*
+Vector2D class for storing two-dimensional spacial vectors.
+*/
+class Vector2D { //TODO move to header
+    public:
+    float x;
+    float y;
+
+    Vector2D(float x, float y) {
+        x = x;
+        y = y;
+    }
+};
+
+/*
+Main.
+*/
 int main() {
     std::cout << "Flocking/swarming\n";
     return 0;
