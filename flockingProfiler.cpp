751e1d038e677c48647216ef07a36b49148d8df1
ashom2 <alekid804@gmail.com>
2024-10-31 13:23:55 +1000

    Added cpu time measurement

--- flockingProfiler.cpp
+++ flockingProfiler.cpp
@@ -15,9 +15,9 @@ const char *profilingFilepath = "ProfilingData.csv";
 /*
 Saves 
 */
-void save(FILE *fptr, int number, double time1) {
+void saveLine(FILE *fptr, int number, double time1, float time2, float time3) {
     // Write row to file
-    fprintf(fptr, "%d, %g\n", number, time1);
+    fprintf(fptr, "%d, %g, %g, %g\n", number, time1, time2, time3);
 }
 
 //TODO check that the parameters are the same - especially numCells (or automate)
@@ -53,7 +53,7 @@ const int numCells_y = 16;
 
     printf("N: %d. Time taken: %g us\n", N, duration_us.count());
 
-    save(fptr, N, duration_us.count());
+    saveLine(fptr, N, duration_us.count());
 }*/
 
 
@@ -61,23 +61,37 @@ const int numCells_y = 16;
 //For GPU
 void ProfileAt(int N, FILE* fptr)
 {
-    //Initialize arrays
-    setVars(512, 512, 64, 0.2, 8, 0.15, 20, 0.05, 0.2, 1, 2, 0.7 * 3.14);
-    init(N);
-    
     // Begin timing
     auto start = std::chrono::high_resolution_clock::now();
 
-    // Do function
-    updateBoids_GPU(N);
+    //Initialize arrays
+    setVars(512, 512, 64, 0.2, 8, 0.15, 20, 0.05, 0.2, 1, 2, 0.7 * 3.14);
+    init(N);
 
     // End timing
     auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::micro> initDuration_us = end - start;
+
+    //printf("N: %d. init time: %g us,     ", N, duration_us.count());
+
+
+    
+    // Begin timing
+    start = std::chrono::high_resolution_clock::now();
+
+    // Do function
+    float kernelTime = updateBoids_GPU(N);
+
+    // End timing
+    end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::micro> duration_us = end - start;
 
-    printf("N: %d. Time taken: %g us\n", N, duration_us.count());
+    printf("N: %d. Time taken: %g us. Kernel time: %g us. CPU time: %g\n", N, duration_us.count(), kernelTime, duration_us.count() - kernelTime);
 
-    save(fptr, N, duration_us.count());
+    saveLine(fptr, N, duration_us.count(), kernelTime, duration_us.count() - kernelTime);
+
+    //Free the boids array memory
+    freeMemory();
 }
 
 
@@ -93,25 +107,25 @@ int main()
     }
 
     // Column headings
-    fprintf(fptr, "N, Time (us)\n");
+    fprintf(fptr, "N, Total time (us), Kernel time (us), CPU time (us)\n");
 
     
 
     //Logarithmic profiling
-    /*
-    int max = 10000;
+    
+    /*int max = 1000000;
     for (int N = 1; N < max; N *= 10) {
         ProfileAt(N, fptr);
         ProfileAt(N * pow(10, 0.25), fptr);
         ProfileAt(N * pow(10, 0.5), fptr);
         ProfileAt(N * pow(10, 0.75), fptr);
     }
-    ProfileAt(max, fptr);
-    */
+    ProfileAt(max, fptr);*/
+    
 
     //Linear profiling
     int max = 1000000;
-    int step = 2500;
+    int step = 5000;
     for (int N = step; N <= max; N += step) {
         ProfileAt(N, fptr);
     }
