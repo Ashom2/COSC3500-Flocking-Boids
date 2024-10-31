8c65067fa8f42a6df1855fb31fbd50dd4af35bae
ashom2 <alekid804@gmail.com>
2024-10-31 15:10:38 +1000

    Added some stuff

---
+++ flockingGPU.cuh
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <list>
+#include <math.h>
+
+void setVars(int _xSize, int _ySize, int _marginSize, 
+        float _turnFactor, 
+        float _avoidRange, float _avoidFactor,
+        float _visualRange, float _matchingFactor,
+        float _cohesionFactor,
+        float _minSpeed, float _maxSpeed,
+        float _formationAngle);
+
+void save(FILE *fptr, int numBoids, int frameNumber);
+
+void init(int numBoids);
+
+__host__ float updateBoids_GPU(int N);
+
+void freeMemory();
